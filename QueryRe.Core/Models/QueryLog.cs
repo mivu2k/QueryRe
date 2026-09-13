63214e61fa4879c15f0ccbf910df47af33de92d6
Muhammad Usman <80588347+mivu2k@users.noreply.github.com>
2026-09-13 17:30:38 +0500

    Delete QueryRe.Core directory

--- QueryRe.Core/Models/QueryLog.cs
+++
@@ -1,9 +0,0 @@
-namespace QueryRe.Core.Models;
-
-public class QueryLog
-{
-    public DateTime Time { get; set; } = DateTime.Now;
-    public string Question { get; set; } = "";
-    public string Sql { get; set; } = "";
-    public bool Success { get; set; }
-}
