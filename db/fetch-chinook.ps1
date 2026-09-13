6234e45be3479dd553a87bbecfa13a8bbde67315
Muhammad Usman <80588347+mivu2k@users.noreply.github.com>
2026-09-13 17:51:06 +0500

    Delete db directory

--- db/fetch-chinook.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-$url = "https://raw.githubusercontent.com/lerocha/chinook-database/master/ChinookDatabase/DataSources/Chinook_SqlServer.sql"
-$output = Join-Path $PSScriptRoot "Chinook_SqlServer.sql"
-
-Invoke-WebRequest -Uri $url -OutFile $output
-Write-Host "Saved Chinook SQL Server script to $output"
